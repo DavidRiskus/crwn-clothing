eaf140d0784390239874f27c08bbc5cf1e8c7315
David Riskus <d.riskus@outlook.com>
2021-03-25 14:50:56 +0000

    updated shop data, Created Utils for grouping duplicates in cart, cartItems in cartReducer

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -18,8 +18,8 @@ const PrieviewCollection = ({ title, items }) => {
                  */}
                 {items
                     .filter((item, idx) => idx < 4)
-                    .map(({ id, ...otherItemProps }) => (
-                        <CollectionItem key={id} {...otherItemProps} />
+                    .map((item) => (
+                        <CollectionItem key={item.id} item={item} />
                     )
                     )}
             </div>
